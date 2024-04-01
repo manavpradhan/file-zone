8c737e663e692bf72ed5c6b77cfb48e6807661ea
manavpradhan <manavpradhan36@gmail.com>
2024-04-01 16:09:13 +0530

    setting up mutation, queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@
 import { Button } from "@/components/ui/button";
 import { SignOutButton, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { SignInButton, useSession } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
   const session = useSession();
+  const createFile = useMutation(api.files.createFile);
+  const files = useQuery(api.files.getFiles);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedOut>
@@ -15,9 +20,19 @@ export default function Home() {
       </SignedOut>
       <SignedIn>
         <SignOutButton>
-          <Button>Sign out</Button>
+          <Button>Sign Out</Button>
         </SignOutButton>
       </SignedIn>
+
+      {files?.length === 0 && <p>no records</p>}
+
+      {files?.map((file)=>{
+        return <div key={file._id}>{file.name}</div>
+      })}
+
+      <Button onClick={()=>{createFile({
+        name: "hello world"
+      })}}>hit me</Button>
     </main>
   );
 }
